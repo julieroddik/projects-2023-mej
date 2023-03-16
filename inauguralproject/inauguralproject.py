e36e96c15d3e8f67d3d9573ce665dc11b6ebd4e2
Emiliedalhoff <tfz818@alumni.ku.dk>
2023-03-16 14:52:48 +0100

    hoho

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,19 +1,3 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
-
 
 from types import SimpleNamespace
 
