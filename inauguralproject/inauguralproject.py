39a9ac99c294e2a9b90389e216d1b292913ffc90
Emiliedalhoff <tfz818@alumni.ku.dk>
2023-03-23 16:47:27 +0100

    Ændringer - Mille

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,4 +1,3 @@
-
 from types import SimpleNamespace
 
 import numpy as np
@@ -44,6 +43,10 @@ class HouseholdSpecializationModelClass:
         sol.beta0 = np.nan
         sol.beta1 = np.nan
 
+        # g. constraints 
+        par.constraints = ({'type': 'ineq', 'fun': lambda x: 24 - (x[0]+x[1])},{'type': 'ineq', 'fun': lambda x: 24 - (x[2]+x[3])})
+
+
     def calc_utility(self,LM,HM,LF,HF):
         """ calculate utility """
 
@@ -54,7 +57,15 @@ class HouseholdSpecializationModelClass:
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
+        if par.sigma == 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+            
+        elif par.sigma == 0:
+            H = np.min(HM, HF)
+           
+        else:
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1))
+        
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
@@ -67,6 +78,7 @@ class HouseholdSpecializationModelClass:
         disutility = par.nu*(TM**epsilon_/epsilon_+TF**epsilon_/epsilon_)
         
         return utility - disutility
+
 
     def solve_discrete(self,do_print=False):
         """ solve model discretely """
@@ -98,6 +110,7 @@ class HouseholdSpecializationModelClass:
         opt.HM = HM[j]
         opt.LF = LF[j]
         opt.HF = HF[j]
+        opt.u = u[j]
 
         # e. print
         if do_print:
@@ -106,15 +119,68 @@ class HouseholdSpecializationModelClass:
 
         return opt
 
-    def solve(self,do_print=False):
+
+    def solve(self, do_print=False):
         """ solve model continously """
+  
+        par = self.par
+        sol = self.sol
+        opt = SimpleNamespace()
 
-        pass    
+        # a. objective function to minimize 
+        def objective(x):
+            return -self.calc_utility(x[0], x[1], x[2], x[3])
+        
+        obj = lambda x: objective(x)
+        guess = [3,5.5,5,4]
+        bounds = [(0,24)]*4
 
+        # b. optimizer
+        result = optimize.minimize(obj,
+                            guess,
+                            method='SLSQP',
+                            bounds=bounds,
+                            constraints=par.constraints)
+        
+        #c. store results
+        opt.LM = result.x[0]
+        opt.HM = result.x[1]
+        opt.LF = result.x[2]
+        opt.HF = result.x[3]
+        opt.u =self.calc_utility(opt.LM, opt.HM, opt.LF, opt.HF)
+
+        #d. print
+        if do_print:
+            for k,v in opt.__dict__.items():
+                print(f'{k} = {v:6.4f}')
+
+        return opt
+        
+      
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
 
-        pass
+        par = self.par
+        sol = self.sol
+
+        #a. loop over relative wage and solve model
+        for i in par.wF_vec:
+            par.wF = i
+            if discrete: # for discrete choice model
+                results = self.solve_discrete()
+            else: # for continous choice model
+                results = self.solve()
+            # 1. find index of argument  
+            j = np.where(par.wF_vec ==i)[0][0]
+
+            # 2. store results
+            sol.LM_vec[j] = results.LM
+            sol.HM_vec[j] = results.HM
+            sol.LF_vec[j] = results.LF
+            sol.HF_vec[j] = results.HF
+
+        return sol
+    
 
     def run_regression(self):
         """ run regression """
@@ -126,10 +192,66 @@ class HouseholdSpecializationModelClass:
         y = np.log(sol.HF_vec/sol.HM_vec)
         A = np.vstack([np.ones(x.size),x]).T
         sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
+
     
-    def estimate(self,alpha=None,sigma=None):
-        """ estimate alpha and sigma """
+    def estimate(self,alpha=None):
+        """ estimate alpha and sigma for variable and fixed alpha """
 
-        pass
+        par = self.par
+        sol = self.sol
 
+        # a. alpha
+        if alpha == None:
+            # 1. objective function to minimize
+            def objective(y):
+                par.alpha = y[1] 
+                par.sigma = y[0] 
+                self.solve_wF_vec()
+                self.run_regression()
+                return (par.beta0_target - sol.beta0)**2 + (par.beta1_target - sol.beta1)**2
+
+            obj = lambda y: objective(y)
+            guess = [0.5]*2
+            bounds = [(-0.00001,1)]*2
+            # 2. optimizer
+            result = optimize.minimize(obj,
+                                guess,
+                                method='Nelder-Mead',
+                                bounds=bounds)
+            # 3. print
+            print(f'alpha = {result.x[1]}')
+            print(f'sigma = {result.x[0]}')
+        else:
+            # 1. objective function to minimize
+            def objective(y):
+                par.alpha = alpha 
+                par.sigma = y[0] 
+                self.solve_wF_vec()
+                self.run_regression()
+                return (par.beta0_target - sol.beta0)**2 + (par.beta1_target - sol.beta1)**2
+
+            obj = lambda y: objective(y)
+            guess = [0.5]
+            bounds = [(-0.00001,1)]
+            # 2. optimizer
+            result = optimize.minimize(obj,
+                                guess,
+                                method='Nelder-Mead',
+                                bounds=bounds)
+          
+            return result
+
+    def plot_illustration(self, plot_dataframe):
+        """ Code to make the illutrations """
+        
+        #a. contruct the figure 
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+
+        #b. adds the variables 
+        ax.plot(plot_dataframe['w ratio'],plot_dataframe['H ratio'])
+        
+        #c. set labels 
+        ax.set_xlabel('$log(\omega_F/\omega_M)$')
+        ax.set_ylabel('$log(H_F/H_M)$')
 
